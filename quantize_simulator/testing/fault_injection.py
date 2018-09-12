e7f15a59a24f1e1ac970fa7efdb77e5de5d43f17
spiritator <leo51leo51leo51@gmail.com>
2018-09-12 19:44:49 +0800

    update

--- quantize_simulator/testing/fault_injection.py
+++ quantize_simulator/testing/fault_injection.py
@@ -11,5 +11,12 @@ import keras
 from keras.models import Sequential, Model
 import numpy as np
 
-def inject_stuck_at_fault(model,layer,x,y,channel,stuck_at):
+def generate_single_stuck_at_fault(original_value,word_width,factorial_bits,fault_bit,stuck_at):
+    if word_width<=factorial_bits-1:
+        raise ValueError('Not enough word width %d for factorial bits %d'%(word_width,factorial_bits))
     
+    if fault_bit<0 or fault_bit>word_width or isinstance(fault_bit,int):
+        raise ValueError('Fault bit must be integer between (include) %d and 0. %d is MSB, 0 is LSB.'%(word_width-1,word_width-1))
+        
+    if stuck_at!=1 or stuck_at!=0:
+        raise ValueError('You must stuck at 0 or 1.')

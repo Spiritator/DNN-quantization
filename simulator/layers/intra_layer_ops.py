479c7e4ccc9acdf28746014f10aeb50618394a5c
Spiritator <spiritator@g.ncu.edu.tw>
2019-02-12 17:23:24 +0800

    intra layer core operation

--- simulator/layers/intra_layer_ops.py
+++ simulator/layers/intra_layer_ops.py
@@ -41,11 +41,11 @@ def QuantizedDenseCore(inputs, kernel, nb, fb, rounding_method):
 ### Reimplemented Conv ###
 ##########################
 # parallel_iterations and swap_memory in tf.while_loops can be adjusted
-def q2dconvolution_op(inputs, filters, quantizer, strides, rate, padding, data_format):
+def QuantizedConv2DCore(inputs, kernel, strides, rate, padding, data_format, nb, fb, rounding_method):
     ''' Reimplementation of the 2D convolution layer.
     Args: 
         inputs:  [batch_size, image_height, image_width, input_channels] 
-        filters: [filter_height, filter_width, input_channels, output_channels]
+        kernel: [kernel_height, kernel_width, input_channels, output_channels]
         quantizer: Quantizer object, has interface '.quantize(tensor)'       
     '''
     PARALLEL_ITERATIONS=1 # number of convolution ops which can run in parallel.
@@ -57,13 +57,13 @@ def q2dconvolution_op(inputs, filters, quantizer, strides, rate, padding, data_f
     batch_size = inputs.shape.dims[0].value
     output = tf.split(inputs,batch_size)
 
-    # prepare filters
-    filter_shape = filters.get_shape()
-    filters=tf.split(filters,filters.shape.dims[3].value,axis=3)
+    # prepare kernel
+    kernel_shape = kernel.get_shape()
+    kernel = tf.split(kernel,kernel.shape.dims[3].value,axis=3)
 
-    # get patch shape, needed for result shape estimation
+    # get patch shape, needed for ofmap shape estimation
     patch = tf.extract_image_patches(output[0], 
-                                           ksizes=(1,filter_shape.dims[0], filter_shape.dims[1],1), 
+                                           ksizes=(1,kernel_shape.dims[0], kernel_shape.dims[1],1), 
                                            strides=strides,
                                            rates=rate,#[1,1,1,1],
                                            padding=padding )
@@ -72,138 +72,104 @@ def q2dconvolution_op(inputs, filters, quantizer, strides, rate, padding, data_f
     # inner loop condition and body.
     # iterates over all output maps
     def inner_cond(index, outputs, output_patch):
-        return index < filter_shape.dims[3].value 
+        return index < kernel_shape.dims[3].value 
 
     def inner_body(index, outputs, output_patch):
-        current_filter = tf.gather(filters, index)
-        current_filter = tf.reshape(current_filter, [1,1,1,patch_shape.dims[3].value])
-        current_filter = tf.tile(current_filter,[1,patch_shape.dims[1].value,patch_shape.dims[2].value,1])  
-        out = tf.multiply(output_patch, current_filter)
-        if quantizer is not None:
-            out = quantizer.quantize(out)     # quantize after multiply
-        out = tf.reduce_sum(out,axis=3,keep_dims=True)
-        if quantizer is not None:
-            out = quantizer.quantize(out)     # quantize after add
-        outputs = tf.concat([outputs,out],3)
+        kernel_tmp = tf.gather(kernel, index)
+        kernel_tmp = tf.reshape(kernel_tmp, [1,1,1,patch_shape.dims[3].value])
+        kernel_tmp = tf.tile(kernel_tmp,[1,patch_shape.dims[1].value,patch_shape.dims[2].value,1])  
+        
+        out_tmp = tf.multiply(output_patch, kernel_tmp)
+        # quantize after multiplication
+        out_tmp = quantize(out_tmp, nb, fb, rounding_method)     
+        
+        out_tmp = tf.reduce_sum(out_tmp,axis=3,keep_dims=True)
+        # quantize after accumulation
+        out_tmp = quantize(out_tmp, nb, fb, rounding_method)     
+        
+        outputs = tf.concat([outputs,out_tmp],3)
+        
         return [tf.add(index,1), outputs, output_patch]
 
     # outer loop condition and body
     # iterates over all batches
-    def outer_cond(batch, result):
+    def outer_cond(batch, ofmap):
         return batch < batch_size
 
-    def outer_body(batch, result):
+    def outer_body(batch, ofmap):
         # extract patch form global 'output'
         output_patch = tf.extract_image_patches(tf.gather(output,batch), 
-                                           ksizes=(1,filter_shape.dims[0], filter_shape.dims[1],1), 
+                                           ksizes=(1,kernel_shape.dims[0], kernel_shape.dims[1],1), 
                                            strides=strides,
                                            rates=rate,#[1,1,1,1],
                                            padding=padding )
-        # prepare inner loop interation variable 'out_filter'
-        out_filter=tf.constant(0)
-        # placeholder 'outputs', results will be concatenated to this tensor. 
+        # prepare inner loop interation variable 'out_kernel'
+        out_kernel=tf.constant(0)
+        # placeholder 'outputs', ofmaps will be concatenated to this tensor. 
         # Remove first element after all elements are computed!
         outputs=tf.constant(0.0,
                             shape=[1, output_patch.shape.dims[1].value,
                             output_patch.shape.dims[2].value, 1])
-        # start inner loop. pass loop iterator, result placeholder and patch. 
-        # Take 2nd element [1] as result!
-        outputs=tf.while_loop( inner_cond, inner_body, [out_filter, outputs, output_patch],
-                shape_invariants=[ out_filter.get_shape(), tf.TensorShape(
+        # start inner loop. pass loop iterator, ofmap placeholder and patch. 
+        # Take 2nd element [1] as ofmap!
+        outputs=tf.while_loop( inner_cond, inner_body, [out_kernel, outputs, output_patch],
+                shape_invariants=[ out_kernel.get_shape(), tf.TensorShape(
                     [1,output_patch.shape.dims[1].value,output_patch.shape.dims[2].value,None]),
                     output_patch.get_shape() ],
                 parallel_iterations=PARALLEL_ITERATIONS,
                 swap_memory=True )[1]
         # concatenate batches (along axis 0).
         # remove first placeholder element from outputs!
-        result= tf.concat([ result,outputs[:,:,:,1:] ], 0)
-        return [tf.add(batch,1), result]
+        ofmap= tf.concat([ ofmap,outputs[:,:,:,1:] ], 0)
+        return [tf.add(batch,1), ofmap]
     
     # main
     # prepare outer loop iteration variable 'batch'
     batch=tf.constant(0)
-    # placeholder 'result', results from inner loop will be concatenated to this tensor.
-    result= tf.constant( 0.0,
+    # placeholder 'ofmap', ofmaps from inner loop will be concatenated to this tensor.
+    ofmap= tf.constant( 0.0,
                           shape=[1, patch_shape.dims[1].value,
-                          patch_shape.dims[2].value, filter_shape.dims[3].value] )
-    # start outer loop. pass 'batch' and 'result'.
-    # Take 2nd element [1] as result!
-    result = tf.while_loop( outer_cond, outer_body, [batch, result],
+                          patch_shape.dims[2].value, kernel_shape.dims[3].value] )
+    # start outer loop. pass 'batch' and 'ofmap'.
+    # Take 2nd element [1] as ofmap!
+    ofmap = tf.while_loop( outer_cond, outer_body, [batch, ofmap],
                 shape_invariants=[ batch.get_shape(), tf.TensorShape(
-                    [None,patch_shape.dims[1].value,patch_shape.dims[2].value,filter_shape.dims[3]]) ],
+                    [None,patch_shape.dims[1].value,patch_shape.dims[2].value,kernel_shape.dims[3]]) ],
                 parallel_iterations=PARALLEL_ITERATIONS,
                 swap_memory=True )[1]
     # remove first element from placeholder!
-    output = result[1:,:,:,:]
-    # output = tf.squeeze(tf.stack(output),axis=[1])
+    output = ofmap[1:,:,:,:]
 
     # setting shape, since partially ignored by while_loops
     output.set_shape([batch_size, 
                         output.shape.dims[1].value,
                         output.shape.dims[2].value,
-                        filter_shape.dims[3].value]) 
+                        kernel_shape.dims[3].value]) 
     return output
 
 # quantized batch normalization calculation
 # tensorflow/python/ops/nn_impl.py
-def qbatch_normalization(inputs,
-                        mean,
-                        variance,
-                        beta,
-                        gamma,
-                        variance_epsilon,
-                        quantizer, 
-                        nb, 
-                        fb, 
-                        rounding_method,
-                        name=None):
-  r"""Batch normalization.
-  As described in http://arxiv.org/abs/1502.03167.
-  Normalizes a tensor by `mean` and `variance`, and applies (optionally) a
-  `scale` \\(\gamma\\) to it, as well as an `offset` \\(\beta\\):
-  \\(\frac{\gamma(x-\mu)}{\sigma}+\beta\\)
-  `mean`, `variance`, `offset` and `scale` are all expected to be of one of two
-  shapes:
-    * In all generality, they can have the same number of dimensions as the
-      input `x`, with identical sizes as `x` for the dimensions that are not
-      normalized over (the 'depth' dimension(s)), and dimension 1 for the
-      others which are being normalized over.
-      `mean` and `variance` in this case would typically be the outputs of
-      `tf.nn.moments(..., keep_dims=True)` during training, or running averages
-      thereof during inference.
-    * In the common case where the 'depth' dimension is the last dimension in
-      the input tensor `x`, they may be one dimensional tensors of the same
-      size as the 'depth' dimension.
-      This is the case for example for the common `[batch, depth]` layout of
-      fully-connected layers, and `[batch, height, width, depth]` for
-      convolutions.
-      `mean` and `variance` in this case would typically be the outputs of
-      `tf.nn.moments(..., keep_dims=False)` during training, or running averages
-      thereof during inference.
-  Args:
-    x: Input `Tensor` of arbitrary dimensionality.
-    mean: A mean `Tensor`.
-    variance: A variance `Tensor`.
-    offset: An offset `Tensor`, often denoted \\(\beta\\) in equations, or
-      None. If present, will be added to the normalized tensor.
-    scale: A scale `Tensor`, often denoted \\(\gamma\\) in equations, or
-      `None`. If present, the scale is applied to the normalized tensor.
-    variance_epsilon: A small float number to avoid dividing by 0.
-    name: A name for this operation (optional).
-  Returns:
-    the normalized, scaled, offset tensor.
-  """
-  with ops.name_scope(name, "batchnorm", [inputs, mean, variance, gamma, beta]):
-    #internal calculation of sqrt is NOT quantized! 
-    coef = quantize( math_ops.sqrt(variance + variance_epsilon), nb, fb, rounding_method )
-    coef = quantize( math_ops.reciprocal(coef), nb, fb, rounding_method )
-    if gamma is not None:
-      coef = quantize(coef*gamma, nb, fb, rounding_method)
-    
-    if beta is not None:
-        const = quantize( beta - quantizer.quantize(mean * coef), nb, fb, rounding_method )
-    else:
-        const = quantize(-mean * coef, nb, fb, rounding_method)
-    result = quantize( quantize(inputs * coef, nb, fb, rounding_method) + const, nb, fb, rounding_method )
-    return result
+def QuantizedBatchNormalizationCore(inputs,
+                                    mean,
+                                    variance,
+                                    beta,
+                                    gamma,
+                                    variance_epsilon,
+                                    quantizer, 
+                                    nb, 
+                                    fb, 
+                                    rounding_method,
+                                    name=None):
+    with ops.name_scope(name, "batchnorm", [inputs, mean, variance, gamma, beta]):
+        coef = quantize( math_ops.sqrt(variance + variance_epsilon), nb, fb, rounding_method )
+        coef = quantize( math_ops.reciprocal(coef), nb, fb, rounding_method )
+        if gamma is not None:
+          coef = quantize(coef*gamma, nb, fb, rounding_method)
+        
+        if beta is not None:
+            const = quantize( beta - quantizer.quantize(mean * coef), nb, fb, rounding_method )
+        else:
+            const = quantize(-mean * coef, nb, fb, rounding_method)
+        output = quantize( quantize(inputs * coef, nb, fb, rounding_method) + const, nb, fb, rounding_method )
+        return output
 

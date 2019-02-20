05df4e5c783afea790df5568b375021e580c3af1
spiritator <leo51leo51leo51@gmail.com>
2019-02-20 20:10:48 +0800

    minor bug fix

--- simulator/quantize_simulator_4C2F_cnn_cifar10.py
+++ simulator/quantize_simulator_4C2F_cnn_cifar10.py
@@ -26,12 +26,15 @@ from approximation.estimate import comp_num_estimate
 # model setup
 
 weight_name='../../cifar10_4C2F_weight.h5'
+batch_size=25
+
 
 # model setup
 # all augments use the same quantize precision
 #model=quantized_4C2F(nbits=8,fbits=4,rounding_method='nearest')
+model=quantized_4C2F(nbits=10,fbits=5,rounding_method='nearest',batch_size=batch_size,intrinsic=False)
 # each augment uses different quantize precision. information list [input, weight, output]
-model=quantized_4C2F(nbits=[12,6,12],fbits=[6,3,6],rounding_method='nearest')
+#model=quantized_4C2F(nbits=[12,6,12],fbits=[6,3,6],rounding_method='nearest')
 model.compile(loss='categorical_crossentropy',optimizer='adam',metrics=['accuracy',top2_acc])
 weight_name=convert_original_weight_layer_name(weight_name)
 model.load_weights(weight_name)
@@ -47,7 +50,7 @@ x_train, x_test, y_train, y_test, class_indices, datagen, input_shape = dataset_
 
 t = time.time()
 
-test_result = model.evaluate(x_test, y_test, verbose=1)
+test_result = model.evaluate(x_test, y_test, verbose=1, batch_size=batch_size)
 
 t = time.time()-t
 
@@ -64,7 +67,7 @@ print('Total # of MAC bits:', computaion_esti['total_MAC_bits'])
 # draw confusion matrix
 
 print('\n')
-prediction = model.predict(x_test, verbose=1)
+prediction = model.predict(x_test, verbose=1, batch_size=batch_size)
 prediction = np.argmax(prediction, axis=1)
 
 show_confusion_matrix(np.argmax(y_test, axis=1),prediction,class_indices,'Confusion Matrix',figsize=(8,6),normalize=False)

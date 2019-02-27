4a9b9b58e20d854a2a2ac08a3b4342cfb934ec06
Spiritator <spiritator@g.ncu.edu.tw>
2019-02-28 00:23:54 +0800

    inference scheme

--- simulator/inference/scheme.py
+++ simulator/inference/scheme.py
@@ -7,21 +7,25 @@ Created on Fri Feb 22 21:41:51 2019
 Plan for multiple inferece setting and write into file
 """
 
-import keras
+import keras, os, csv
 from keras.utils import multi_gpu_model
+from utils_tool.weight_conversion import convert_original_weight_layer_name
 from utils_tool.dataset_setup import dataset_setup
-from metrics.topk_metrics import top2_acc,top3_acc,top5_acc
+from metrics.topk_metrics import *
 import time
 
-def inference_scheme(model_func, model_augment, compile_augment, dataset_augment, multi_gpu=False, gpu_num=2):
+def inference_scheme(model_func, model_augment, compile_augment, dataset_augment, result_save_file, weight_load=False, weight_name=None, multi_gpu=False, gpu_num=2):
     """Take scheme as input and run different setting of inference automaticly. Write the results into a csv file.
 
     # Arguments
         model_func: The callable function which returns a DNN model. (Keras funtional model API recommmanded).
         model_augment: List of Dictionarys. The augments for DNN model function.
-        compile_augment: List of Dictionarys. The augments for model compile augment.
-        dataset_augment: List of Dictionarys. The augments for dataset setup.
-        multi_gpu: Using multi GPU inference or not.
+        compile_augment: Dictionary. The augments for model compile augment.
+        dataset_augment: Dictionary. The augments for dataset setup.
+        result_save_file: String. The file and directory to the result csv file.
+        weight_load: Bool. Need load weight proccess outside model_func or not.
+        weight_name: String. The weight file to load. (if weight_load is True)
+        multi_gpu: Bool. Using multi GPU inference or not.
         gpu_num: Integer. The number of GPUs in your system setup. (for multi_gpu = True only)
 
     # Returns
@@ -30,12 +34,13 @@ def inference_scheme(model_func, model_augment, compile_augment, dataset_augment
     if not callable(model_func):
         raise TypeError('The model_func augment must be a callable function which returns a Keras DNN model.')
         
+        
     print('preparing dataset...')
     x_train, x_test, y_train, y_test, class_indices, datagen, input_shape = dataset_setup( **dataset_augment)
     print('dataset ready')
 
     
-    for scheme_num in len(model_augment):
+    for scheme_num in range(len(model_augment)):
         print('Running inference scheme %d/%d'%(scheme_num,len(model_augment)))
         
         print('Building model...')
@@ -43,6 +48,10 @@ def inference_scheme(model_func, model_augment, compile_augment, dataset_augment
         t = time.time()
 
         model=model_func( **model_augment[scheme_num])
+        
+        if weight_load:
+            weight_name=convert_original_weight_layer_name(weight_name)
+            model.load_weights(weight_name)
         
         model.summary()
 
@@ -86,5 +95,25 @@ def inference_scheme(model_func, model_augment, compile_augment, dataset_augment
         print('\nTest loss:', test_result[0])
         for i in range(1,len(test_result)):
             print('Test metric %d :'%i, test_result[i])
-        
+            
+        if scheme_num is 0:     
+            with open(result_save_file, 'w', newline='') as csvfile:
+                fieldnames=['loss']
+                test_result_dict={'loss':test_result[0]}
+                for i in range(1,len(test_result)):
+                    fieldnames.append('metric %d'%i)
+                    test_result_dict['metric %d'%i]=test_result[i]
+                writer=csv.DictWriter(csvfile, fieldnames=fieldnames)
+                writer.writeheader()
+                writer.writerow(test_result_dict)
+        else:
+            with open(result_save_file, 'a', newline='') as csvfile:
+                    test_result_dict={'loss':test_result[0]}
+                    for i in range(1,len(test_result)):
+                        test_result_dict['metric %d'%i]=test_result[i]
+                    writer=csv.DictWriter(csvfile, fieldnames=fieldnames)
+                    writer.writerow(test_result_dict)
+                    
+        print('\n===============================================\n')
+
 
--- simulator/scheme_example.py
+++ simulator/scheme_example.py
@@ -7,4 +7,51 @@ Created on Wed Feb 27 16:46:08 2019
 An example of using inference scheme to arange analysis and save result.
 """
 
+from inference.scheme import inference_scheme
+#from models.model_library import quantized_lenet5
+from models.resnet50 import QuantizedResNet50, preprocess_input
+from metrics.topk_metrics import top2_acc,top5_acc
+
+#result_save_file='../../test_result/mnist_lenet5_hybrid.csv'
+#weight_name='../../mnist_lenet5_weight.h5'
+#batch_size=25
+#
+#model_augment=[{'nbits':16,'fbits':8,'rounding_method':'nearest','batch_size':batch_size,'quant_mode':'hybrid'},
+#               {'nbits':14,'fbits':7,'rounding_method':'nearest','batch_size':batch_size,'quant_mode':'hybrid'},
+#               {'nbits':12,'fbits':6,'rounding_method':'nearest','batch_size':batch_size,'quant_mode':'hybrid'},
+#               {'nbits':10,'fbits':5,'rounding_method':'nearest','batch_size':batch_size,'quant_mode':'hybrid'},
+#               {'nbits':8,'fbits':4,'rounding_method':'nearest','batch_size':batch_size,'quant_mode':'hybrid'}]
+#
+#compile_augment={'loss':'categorical_crossentropy','optimizer':'adam','metrics':['accuracy',top2_acc]}
+#
+#dataset_augment={'dataset':'mnist'}
+#
+#
+#inference_scheme(quantized_lenet5, model_augment, compile_augment, dataset_augment, result_save_file, weight_load=True, weight_name=weight_name)
+
+#%%
+
+result_save_file='../../test_result/imagenet_Resnet50_extrinsic.csv'
+weight_name='../../resnet50_weights_tf_dim_ordering_tf_kernels.h5'
+img_width, img_height = 224, 224
+class_number=1000
+batch_size=40
+validation_data_dir = '../../../dataset/imagenet_val_imagedatagenerator_setsize_2'
+nb_validation_samples = 50000
+
+model_augment=[{'weights':weight_name,'nbits':28,'fbits':10,'BN_nbits':28,'BN_fbits':10,'rounding_method':'nearest','batch_size':batch_size,'quant_mode':'extrinsic'},
+               {'weights':weight_name,'nbits':28,'fbits':10,'BN_nbits':28,'BN_fbits':10,'rounding_method':'nearest','batch_size':batch_size,'quant_mode':'extrinsic'},
+               {'weights':weight_name,'nbits':28,'fbits':10,'BN_nbits':28,'BN_fbits':10,'rounding_method':'nearest','batch_size':batch_size,'quant_mode':'extrinsic'},
+               {'weights':weight_name,'nbits':28,'fbits':10,'BN_nbits':28,'BN_fbits':10,'rounding_method':'nearest','batch_size':batch_size,'quant_mode':'extrinsic'},
+               {'weights':weight_name,'nbits':28,'fbits':10,'BN_nbits':28,'BN_fbits':10,'rounding_method':'nearest','batch_size':batch_size,'quant_mode':'extrinsic'},
+               {'weights':weight_name,'nbits':28,'fbits':10,'BN_nbits':28,'BN_fbits':10,'rounding_method':'nearest','batch_size':batch_size,'quant_mode':'extrinsic'},
+               {'weights':weight_name,'nbits':28,'fbits':10,'BN_nbits':28,'BN_fbits':10,'rounding_method':'nearest','batch_size':batch_size,'quant_mode':'extrinsic'},
+               ]
+
+compile_augment={'loss':'categorical_crossentropy','optimizer':'adam','metrics':['accuracy',top5_acc]}
+
+dataset_augment={'dataset':'ImageDataGenerator','img_rows':img_width,'img_cols':img_height,'batch_size':batch_size,'data_augmentation':False,'data_dir':validation_data_dir,'preprocessing_function':preprocess_input}
+
+
+inference_scheme(QuantizedResNet50, model_augment, compile_augment, dataset_augment, result_save_file, multi_gpu=True, gpu_num=2)
 

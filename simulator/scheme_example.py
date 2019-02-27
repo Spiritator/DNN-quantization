3b86cc93f44c942c8780a126be2af2d2ba6727c9
Spiritator <spiritator@g.ncu.edu.tw>
2019-02-28 00:28:38 +0800

    inference scheme

--- simulator/scheme_example.py
+++ simulator/scheme_example.py
@@ -39,13 +39,17 @@ batch_size=40
 validation_data_dir = '../../../dataset/imagenet_val_imagedatagenerator_setsize_2'
 nb_validation_samples = 50000
 
-model_augment=[{'weights':weight_name,'nbits':28,'fbits':10,'BN_nbits':28,'BN_fbits':10,'rounding_method':'nearest','batch_size':batch_size,'quant_mode':'extrinsic'},
-               {'weights':weight_name,'nbits':28,'fbits':10,'BN_nbits':28,'BN_fbits':10,'rounding_method':'nearest','batch_size':batch_size,'quant_mode':'extrinsic'},
-               {'weights':weight_name,'nbits':28,'fbits':10,'BN_nbits':28,'BN_fbits':10,'rounding_method':'nearest','batch_size':batch_size,'quant_mode':'extrinsic'},
-               {'weights':weight_name,'nbits':28,'fbits':10,'BN_nbits':28,'BN_fbits':10,'rounding_method':'nearest','batch_size':batch_size,'quant_mode':'extrinsic'},
-               {'weights':weight_name,'nbits':28,'fbits':10,'BN_nbits':28,'BN_fbits':10,'rounding_method':'nearest','batch_size':batch_size,'quant_mode':'extrinsic'},
-               {'weights':weight_name,'nbits':28,'fbits':10,'BN_nbits':28,'BN_fbits':10,'rounding_method':'nearest','batch_size':batch_size,'quant_mode':'extrinsic'},
-               {'weights':weight_name,'nbits':28,'fbits':10,'BN_nbits':28,'BN_fbits':10,'rounding_method':'nearest','batch_size':batch_size,'quant_mode':'extrinsic'},
+model_augment=[{'weights':weight_name,'nbits':28,'fbits':14,'BN_nbits':28,'BN_fbits':14,'rounding_method':'nearest','batch_size':batch_size,'quant_mode':'extrinsic'},
+               {'weights':weight_name,'nbits':26,'fbits':13,'BN_nbits':26,'BN_fbits':13,'rounding_method':'nearest','batch_size':batch_size,'quant_mode':'extrinsic'},
+               {'weights':weight_name,'nbits':24,'fbits':12,'BN_nbits':24,'BN_fbits':12,'rounding_method':'nearest','batch_size':batch_size,'quant_mode':'extrinsic'},
+               {'weights':weight_name,'nbits':22,'fbits':11,'BN_nbits':22,'BN_fbits':11,'rounding_method':'nearest','batch_size':batch_size,'quant_mode':'extrinsic'},
+               {'weights':weight_name,'nbits':20,'fbits':10,'BN_nbits':20,'BN_fbits':10,'rounding_method':'nearest','batch_size':batch_size,'quant_mode':'extrinsic'},
+               {'weights':weight_name,'nbits':18,'fbits':9,'BN_nbits':18,'BN_fbits':9,'rounding_method':'nearest','batch_size':batch_size,'quant_mode':'extrinsic'},
+               {'weights':weight_name,'nbits':16,'fbits':8,'BN_nbits':16,'BN_fbits':8,'rounding_method':'nearest','batch_size':batch_size,'quant_mode':'extrinsic'},
+               {'weights':weight_name,'nbits':14,'fbits':7,'BN_nbits':14,'BN_fbits':7,'rounding_method':'nearest','batch_size':batch_size,'quant_mode':'extrinsic'},
+               {'weights':weight_name,'nbits':12,'fbits':6,'BN_nbits':12,'BN_fbits':6,'rounding_method':'nearest','batch_size':batch_size,'quant_mode':'extrinsic'},
+               {'weights':weight_name,'nbits':10,'fbits':5,'BN_nbits':10,'BN_fbits':5,'rounding_method':'nearest','batch_size':batch_size,'quant_mode':'extrinsic'},
+               {'weights':weight_name,'nbits':8,'fbits':4,'BN_nbits':8,'BN_fbits':4,'rounding_method':'nearest','batch_size':batch_size,'quant_mode':'extrinsic'},
                ]
 
 compile_augment={'loss':'categorical_crossentropy','optimizer':'adam','metrics':['accuracy',top5_acc]}

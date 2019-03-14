476d863dd4bb6bbfdcfd58c97a409a3cd2c5d2a2
Spiritator <spiritator@g.ncu.edu.tw>
2019-03-14 16:59:41 +0800

    memory mapping implement

--- simulator/memory/tile.py
+++ simulator/memory/tile.py
@@ -10,7 +10,7 @@ DNN tiling for memory fault mapping
 import numpy as np
 
 class tile:
-    def __init__(self, Tm, Tn, Tr, Tc, wl=32, row_prior=[], col_prior=[]):
+    def __init__(self, Tm, Tn, Tr, Tc, is_fmap=True, wl=32, row_prior=[], col_prior=[]):
         """The tile of a DNN feature map or weights
 
         # Arguments
@@ -27,20 +27,39 @@ class tile:
         self.Tn=Tn
         self.Tr=Tr
         self.Tc=Tc
+        self.is_fmap=is_fmap
         self.wl=wl
         self.row_prior=row_prior
         self.col_prior=col_prior
+        self.prior_list=['Tm','Tn','Tr','Tc']
         
     def check_prior(self):
         if not isinstance(self.row_prior,list) or not isinstance(self.col_prior,list) or len(self.row_prior)!=4 or len(self.col_prior)!=4:
             raise ValueError('The augment row_prior and col_prior must be in list dtype and have length 4 but got length %d and %d'%(len(self.row_prior),len(self.col_prior)))
                 
-        prior_list=['Tm','Tn','Tr','Tc']
         for i in range(4):
-            if self.row_prior[i] not in prior_list:
-                raise ValueError('The augment row_prior must be in list %s'%(str(prior_list)))
-            if self.col_prior[i] not in prior_list:
-                raise ValueError('The augment col_prior must be in list %s'%(str(prior_list)))
+            if self.row_prior[i] not in self.prior_list:
+                raise ValueError('The augment row_prior must be in list %s'%(str(self.prior_list)))
+            if self.col_prior[i] not in self.prior_list:
+                raise ValueError('The augment col_prior must be in list %s'%(str(self.prior_list)))
+    
+    def priorexchange(self,prior):
+        if prior not in self.prior_list:
+            raise ValueError('The augment row_prior must be in list %s'%(str(self.prior_list)))
+            
+        if self.is_fmap:
+            axis_idex=[3,0,1,2]
+        else:
+            axis_idex=[2,3,0,1]
+            
+        if prior is 'Tm':
+            return self.Tm,axis_idex[0]
+        elif prior is 'Tn':
+            return self.Tn,axis_idex[1]
+        elif prior is 'Tr':
+            return self.Tr,axis_idex[2]
+        elif prior is 'Tc':
+            return self.Tc,axis_idex[3]
         
     def get_numtag_bitmap(self,bitmap,addr):
         if len(addr)!=2:
@@ -51,7 +70,12 @@ class tile:
         if len(coor)!=4:
             raise ValueError('The length of coordinate Tuple in tile must be 4 but got %d.'%(len(coor)))
            
-        #row_addr=
+        id_col_expand=0
+        for i in reversed(range(4)):
+            T_size,T_index=self.priorexchange(self.col_prior[i])
+            id_col_expand+=T_size*coor[T_index]
+            
+        row_addr=id_col_expand % bitmap.col
 
     def fault_dict_bitmap2tile(self,bitmap,row_prior=None,col_prior=None):
         """Mapping the fault on the memory bitmap to tile coordinate

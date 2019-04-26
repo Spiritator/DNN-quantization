2f95453fb37cc51259ca6f18cd8bd50b862b1cb1
Spiritator <spiritator@g.ncu.edu.tw>
2019-04-26 12:05:22 +0800

    bug fix

--- simulator/utils_tool/weight_conversion.py
+++ simulator/utils_tool/weight_conversion.py
@@ -96,7 +96,7 @@ def convert_original_weight_layer_name(original_weight_name,quantized_weight_nam
             quantized_layer.attrs.create('weight_names',[('quantized_'+temp).encode('utf8') for temp in weight_names])
         else:
             quantized_layer.attrs.create('weight_names',[temp.encode('utf8') for temp in weight_names])
-        if len(weight_names) is not 0:
+        if len(weight_names) != 0:
             quantized_sublayer = quantized_layer.create_group(quantized_layer_names[layer_iter])
         
         for weight_iter, weight_name in enumerate(weight_names):
@@ -147,7 +147,7 @@ def quantize_weight(original_weight_name, weight_bit_width, weight_factorial_bit
         weight_values = [np.asarray(o_group[weight_name]) for weight_name in weight_names]
         quantized_layer = q_weight_f.create_group(layer_names[layer_iter])
         quantized_layer.attrs.create('weight_names',[temp.encode('utf8') for temp in weight_names])
-        if len(weight_names) is not 0:
+        if len(weight_names) != 0:
             quantized_sublayer = quantized_layer.create_group(layer_names[layer_iter])
         
         for weight_iter, weight_name in enumerate(weight_names):
@@ -308,5 +308,136 @@ def fuse_BN_weight(original_weight_name,fused_weight_name=None):
     
     return fused_weight_name
 
+
+# for the unusual resnet50 weight file
+def fuse_BN_weight_beta(original_weight_name,layer_names=None,fused_weight_name=None):
+    
+    #================================================================
+    possible_BN_name_list = ['bn','BN','BatchNormalization','batch']
+    epsilon = 1e-3
+    #================================================================
+
+
+    o_weight_f = h5py.File(original_weight_name,'r')
+    if fused_weight_name is None:
+        fused_weight_name=original_weight_name[:-3]+('_fused_BN.h5')
+        if os.path.isfile(fused_weight_name):
+            print('Fused BN weight already exist skip conversion and continue...')
+            return fused_weight_name
+        else:
+            f_weight_f = h5py.File(fused_weight_name,'w')
+    else:
+        if os.path.isfile(fused_weight_name):
+            print('Fused BN weight already exist skip conversion and continue...')
+            return fused_weight_name
+        else:
+            f_weight_f = h5py.File(fused_weight_name,'w')
+        
+        
+    if 'keras_version' in o_weight_f.attrs:
+        original_keras_version = o_weight_f.attrs['keras_version'].decode('utf8')
+    else:
+        original_keras_version = '1'
+    if 'backend' in o_weight_f.attrs:
+        original_backend = o_weight_f.attrs['backend'].decode('utf8')
+    else:
+        original_backend = 'None'
+        
+    if layer_names is None:
+        layer_names = load_attributes_from_hdf5_group(o_weight_f, 'layer_names')
+    fused_layer_names = []
+    for layer_iter,layer_name in enumerate(layer_names[:-1]):
+        pred_list=[(keyword in layer_names[layer_iter+1]) for keyword in possible_BN_name_list]
+        if any(pred_list):
+            fused_layer_names.append(layer_name)
+        
+    new_layer_names = []
+    bn_layer_names = []
+    for layer_name in layer_names:
+        pred_list=[(keyword in layer_name) for keyword in possible_BN_name_list]
+        if any(pred_list):
+            bn_layer_names.append(layer_name)
+        else:
+            new_layer_names.append(layer_name)
+
+            
+            
+    f_weight_f.attrs.create('layer_names',[temp.encode('utf8') for temp in new_layer_names])
+    f_weight_f.attrs.create('backend',original_backend.encode('utf8'))
+    f_weight_f.attrs.create('keras_version',original_keras_version.encode('utf8'))
+    
+    for layer_iter, layer_name in enumerate(layer_names):
+        if layer_name not in bn_layer_names:
+            o_group = o_weight_f[layer_name]
+            weight_names = load_attributes_from_hdf5_group(o_group, 'weight_names')
+            weight_values = [np.asarray(o_group[weight_name]) for weight_name in weight_names]
+            fused_layer = f_weight_f.create_group(layer_names[layer_iter])
+            fused_layer.attrs.create('weight_names',[temp.encode('utf8') for temp in weight_names])
+            if len(weight_names) != 0:
+                fused_sublayer = fused_layer#.create_group(layer_names[layer_iter])
+            
+            if layer_name in fused_layer_names:
+                bn_o_group = o_weight_f[layer_names[layer_iter+1]]
+                bn_weight_names = load_attributes_from_hdf5_group(bn_o_group, 'weight_names')
+                bn_weight_values = [np.asarray(bn_o_group[weight_name]) for weight_name in bn_weight_names]
+                
+                if weight_values[0].shape[-1]==len(bn_weight_values[0]):
+                    # normal circumstance output channel at last dimension
+                    shape_tmp1 = []
+                    shape_tmp2 = []
+                    for i in range(len(weight_values[0].shape)-1):
+                        shape_tmp1.append(1)
+                        shape_tmp2.append(weight_values[0].shape[i])
+                    shape_tmp1.append(len(bn_weight_values[0]))
+                    shape_tmp2.append(1)
+                elif weight_values[0].shape[-2]==len(bn_weight_values[0]) and weight_values[0].shape[-1]==1:
+                    # depthwise convolution 
+                    shape_tmp1 = []
+                    shape_tmp2 = []
+                    for i in range(len(weight_values[0].shape)-2):
+                        shape_tmp1.append(1)
+                        shape_tmp2.append(weight_values[0].shape[i])
+                    shape_tmp1.append(len(bn_weight_values[0]))
+                    shape_tmp1.append(1)
+                    shape_tmp2.append(1)
+                    shape_tmp2.append(1)
+                    
+                gamma_tmp = bn_weight_values[0]
+                beta_tmp = bn_weight_values[1]
+                mean_tmp = bn_weight_values[2]
+                variance_tmp = bn_weight_values[3]
+                
+                coef = np.divide(gamma_tmp,np.sqrt(variance_tmp+epsilon))
+                const = beta_tmp-np.multiply(mean_tmp,coef)
+                
+                # for those layer without bias
+                if len(weight_names)==1:
+                    fused_bias = const
+                else:
+                    fused_bias = np.multiply(coef,weight_values[1])+const
+                
+                coef = np.tile(np.reshape(coef,shape_tmp1),shape_tmp2)
+                
+                fused_kernel = np.multiply(weight_values[0],coef)
+                
+                    
+                fused_sublayer.create_dataset(weight_names[0],weight_values[0].shape,weight_values[0].dtype,fused_kernel)
+                # for those layer without bias
+                if len(weight_names)==1:
+                    fused_layer.attrs.create('weight_names',[weight_names[0].encode('utf8'),weight_names[0].replace('kernel','bias').encode('utf8')])
+                    fused_sublayer.create_dataset(weight_names[0].replace('kernel','bias'),bn_weight_values[0].shape,bn_weight_values[0].dtype,fused_bias)
+                else:
+                    fused_sublayer.create_dataset(weight_names[1],weight_values[1].shape,weight_values[1].dtype,fused_bias)
+
+                        
+            else:
+                for weight_iter, weight_name in enumerate(weight_names):
+                    fused_sublayer.create_dataset(weight_name,weight_values[weight_iter].shape,weight_values[weight_iter].dtype,weight_values[weight_iter])
+        
+        
+    o_weight_f.close()
+    f_weight_f.close()
+    
+    return fused_weight_name
 
 

407610daf894f19e744165aa48784254687bdbe4
Spiritator <spiritator@g.ncu.edu.tw>
2019-06-09 20:09:15 +0800

    Update example_plot_FT_metrics.py

--- simulator/example_plot_FT_metrics.py
+++ simulator/example_plot_FT_metrics.py
@@ -21,17 +21,18 @@ stat_data=plot_FT_analysis(stat_dir='../../test_result/'+stat_folder_dir)
 
 relative_dir='../../test_result/'
 stat_data_list=list()
-stat_vs_folders=['cifar10_4C2F_model_fault_rate_fmap','cifar10_4C2F_model_fault_rate_10_fmap']
+stat_vs_folders=['cifar10_4C2F_memory_fault_rate_small_less_fmap','cifar10_4C2F_memory_fault_rate_small_fmap','cifar10_4C2F_memory_fault_rate_small_full_fmap']
 
 for dirr in stat_vs_folders:
     stat_data_list.append(make_FT_report_csv(relative_dir+dirr,None,write_csv=False))
 
-pic_save_dir='vs_cifar10_4C2F_model_fault_rate_fmap_16v10'
+pic_save_dir='vs_cifar10_4C2F_memory_fault_rate_fmap_fullness'
 
 color_dict_list=[{'max':'lightblue','min':'lightblue','avg':'blue','var':'darkgray'},
-                 {'max':'peachpuff','min':'peachpuff','avg':'red','var':'darkgray'}]
+                 {'max':'peachpuff','min':'peachpuff','avg':'red','var':'darkgray'},
+                 {'max':'lightgreen','min':'lightgreen','avg':'green','var':'darkgray'}]
 
-label_list=['(S,16,12)','(S,10,6)']
+label_list=['26.24% 25.6KB','44.22% 25.6KB','59.91% 25.6KB']
 
 plot_FT_analysis_multiple(stat_data_list,relative_dir+pic_save_dir,color_dict_list,label_list)
 

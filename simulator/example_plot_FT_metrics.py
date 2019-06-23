2759b6848397cf53616bb0aba0d64b525fd5d4c4
Spiritator <spiritator@g.ncu.edu.tw>
2019-06-23 20:28:57 +0800

    resnet mem fault sim

--- simulator/example_plot_FT_metrics.py
+++ simulator/example_plot_FT_metrics.py
@@ -12,7 +12,7 @@ from utils_tool.plot import make_FT_report_csv,plot_FT_analysis,plot_FT_analysis
 #%%
 # plot 
 
-stat_folder_dir='imagenet_mobilenet_memory_fault_rate'
+stat_folder_dir='imagenet_mobilenet_memory_fault_rate_fmap'
 stat_data=make_FT_report_csv('../../test_result/'+stat_folder_dir,stat_folder_dir)
 stat_data=plot_FT_analysis(stat_dir='../../test_result/'+stat_folder_dir)
 
@@ -21,19 +21,19 @@ stat_data=plot_FT_analysis(stat_dir='../../test_result/'+stat_folder_dir)
 
 relative_dir='../../test_result/'
 stat_data_list=list()
-stat_vs_folders=['mnist_lenet5_model_fault_rate_fmap','cifar10_4C2F_model_fault_rate_10_fmap','imagenet_mobilenet_model_fault_rate_fmap','imagenet_resnet_model_fault_rate_fmap']
+stat_vs_folders=['mnist_lenet5_memory_fault_rate_fmap','cifar10_4C2F_memory_fault_rate_small_fmap','imagenet_mobilenet_memory_fault_rate_fmap']#,'imagenet_resnet_model_fault_rate_fmap']
 
 for dirr in stat_vs_folders:
     stat_data_list.append(make_FT_report_csv(relative_dir+dirr,None,write_csv=False))
 
-pic_save_dir='vs_model_fault_rate_4net_fmap'
+pic_save_dir='vs_memory_fault_rate_4net_fmap'
 
 color_dict_list=[{'max':'lightblue','min':'lightblue','avg':'blue','var':'darkgray'},
                  {'max':'peachpuff','min':'peachpuff','avg':'red','var':'darkgray'},
-                 {'max':'lightgreen','min':'lightgreen','avg':'green','var':'darkgray'},
-                 {'max':'thistle','min':'thistle','avg':'purple','var':'darkgray'}]
+                 {'max':'lightgreen','min':'lightgreen','avg':'green','var':'darkgray'},]
+                 #{'max':'thistle','min':'thistle','avg':'purple','var':'darkgray'}]
 
-label_list=['lenet (S,8,3)','4C2F (S,10,6)','mobilenet (S,16,9)','resnet (S,16,12-8)']
+label_list=['lenet 6.25KB','4C2F 25.6KB','mobilenet 260KB']#,'resnet (S,16,12-8)']
 
 plot_FT_analysis_multiple(stat_data_list,relative_dir+pic_save_dir,color_dict_list,label_list)
 

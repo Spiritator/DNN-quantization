b76d16b669a022c88811f6f51d2c9fa013798ef7
spiritator <leo51leo51leo51@gmail.com>
2019-07-27 19:52:37 +0800

    layer by layer BER implement

--- scheme_4C2F_model_fault_layer_by_layer.py
+++ scheme_4C2F_model_fault_layer_by_layer.py
@@ -9,7 +9,7 @@ Using inference scheme to arange analysis and save result layer by layer.
 import os
 
 from simulator.inference.scheme import inference_scheme
-from simulator.models.model_library import quantized_lenet5
+from simulator.models.model_library import quantized_4C2F
 from simulator.metrics.topk_metrics import top2_acc
 from simulator.metrics.FT_metrics import acc_loss,relative_acc,pred_miss,top2_pred_miss,conf_score_vary_10,conf_score_vary_50
 from keras.losses import categorical_crossentropy
@@ -21,24 +21,24 @@ from simulator.inference.scheme import gen_test_round_list
 #%%
 # setting parameter
 
-result_save_folder='../test_result/mnist_lenet5_model_fault_rate_lbl'
-weight_name='../mnist_lenet5_weight.h5'
-model_word_length=8
-model_fractional_bit=3
+result_save_folder='../test_result/cifar10_4C2F_model_fault_rate_lbl'
+weight_name='../cifar10_4C2FBN_weight_fused_BN.h5'
+model_word_length=16
+model_fractional_bit=12
 batch_size=20
 # model fault simulation parameter
 test_round_upper_bound=200
-test_round_lower_bound=50
+test_round_lower_bound=10
 
 #%%
 
 # model for get configuration
 def call_model():
-    return quantized_lenet5(nbits=model_word_length,
-                            fbits=model_fractional_bit,
-                            batch_size=batch_size,
-                            quant_mode=None)
-
+    return quantized_4C2F(nbits=model_word_length,
+                         fbits=model_fractional_bit,
+                         batch_size=batch_size,
+                         quant_mode=None)
+    
 # layer by layer information
 ref_model=call_model()
 param_size_report=get_model_param_size(ref_model,batch_size)
@@ -52,9 +52,9 @@ for j in range(len(ref_model.layers)):
 # test
 compile_augment={'loss':'categorical_crossentropy','optimizer':'adam','metrics':['accuracy',top2_acc]}
 
-dataset_augment={'dataset':'mnist'}
+dataset_augment={'dataset':'cifar10'}
 
-FT_augment={'model_name':'lenet','loss_function':categorical_crossentropy,'metrics':['accuracy',top2_acc,acc_loss,relative_acc,pred_miss,top2_pred_miss,conf_score_vary_10,conf_score_vary_50]}    
+FT_augment={'model_name':'4c2f','loss_function':categorical_crossentropy,'metrics':['accuracy',top2_acc,acc_loss,relative_acc,pred_miss,top2_pred_miss,conf_score_vary_10,conf_score_vary_50]}    
 
 for layer_id in param_layers:
     input_bits=param_size_report['input_bits'][layer_id]
@@ -70,7 +70,7 @@ for layer_id in param_layers:
     
     if not os.path.isdir(result_save_folder+'/'+str(layer_id)):
         os.mkdir(result_save_folder+'/'+str(layer_id))
-    
+
     for test_rounds,fr in enumerate(fault_rate_list):
         print('|=|=|=|=|=|=|=|=|=|=|=|=|=|=|=|=|=|=|=|=|=|=|=|=|=|=|')
         print('|=|        Test Bit Fault Rate %s'%str(fr))
@@ -118,8 +118,8 @@ for layer_id in param_layers:
                                   'ofmap_fault_dict_list':model_ofmap_fdl,
                                   'weight_fault_dict_list':model_weight_fdl})
     
-        result_save_file=result_save_folder+'/'+str(layer_id)+'/'+str(fr)+'.csv'
-        inference_scheme(quantized_lenet5, 
+        result_save_file=result_save_folder+'/'+str(fr)+'.csv'
+        inference_scheme(quantized_4C2F, 
                          model_augment, 
                          compile_augment, 
                          dataset_augment, 
@@ -127,7 +127,7 @@ for layer_id in param_layers:
                          weight_load=True, 
                          weight_name=weight_name, 
                          FT_evaluate=True, 
-                         FT_augment=FT_augment, 
+                         FT_augment=FT_augment,
                          name_tag='fault rate '+str(fr))
 
 

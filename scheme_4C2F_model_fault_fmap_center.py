563e7132a0438276e66a2eff07f97f17d55214e9
Spiritator <spiritator@g.ncu.edu.tw>
2019-08-01 11:37:28 +0800

    fmap center and edge FT implement

--- scheme_4C2F_model_fault_fmap_center.py
+++ scheme_4C2F_model_fault_fmap_center.py
@@ -9,7 +9,7 @@ Using inference scheme to arange analysis and save result. Evaluate the FT diffe
 import os
 
 from simulator.inference.scheme import inference_scheme
-from simulator.models.model_library import quantized_lenet5
+from simulator.models.model_library import quantized_4C2F
 from simulator.metrics.topk_metrics import top2_acc
 from simulator.metrics.FT_metrics import acc_loss,relative_acc,pred_miss,top2_pred_miss,conf_score_vary_10,conf_score_vary_50
 from keras.losses import categorical_crossentropy
@@ -18,14 +18,14 @@ from simulator.testing.fault_list import generate_model_stuck_fault
 #%%
 # setting parameter
 
-result_save_folder='../test_result/mnist_lenet5_model_fault_rate_fmc'
-weight_name='../mnist_lenet5_weight.h5'
-model_word_length=8
-model_fractional_bit=3
+result_save_folder='../test_result/cifar10_4C2F_model_fault_rate_fmc'
+weight_name='../cifar10_4C2FBN_weight_fused_BN.h5'
+model_word_length=10
+model_fractional_bit=6
 batch_size=20
 # model fault simulation parameter
-fault_rate_list=  [5e-7,1e-6,2e-6,5e-6,1e-5,2e-5,5e-5,1e-4,2e-4,5e-4,1e-3,2e-3,5e-3,1e-2,2e-2,5e-2,1e-1]
-test_rounds_lists=[200 ,200 ,200 ,200 ,200 ,200 ,200 ,200 ,200 ,200 ,200 ,100 ,100 ,100 ,100 ,50  ,50  ]
+fault_rate_list=  [5e-8,1e-7,2e-7,5e-7,1e-6,2e-6,5e-6,1e-5,2e-5,5e-5,1e-4,2e-4,5e-4,1e-3,2e-3,5e-3,1e-2,2e-2,5e-2,1e-1]
+test_rounds_lists=[200 ,200 ,200 ,200 ,200 ,200 ,200 ,200 ,200 ,200 ,200 ,100 ,100 ,100 ,100 ,50  ,50  ,50  ,10  ,10  ]
 # different level of concentration on center of feature maps
 concentration_list=[0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1]
 
@@ -33,24 +33,24 @@ concentration_list=[0, 0.1, 0.2, 0.3, 0.4, 0.5, 0.6, 0.7, 0.8, 0.9, 1]
 
 # model for get configuration
 def call_model():
-    return quantized_lenet5(nbits=model_word_length,
-                            fbits=model_fractional_bit,
-                            batch_size=batch_size,
-                            quant_mode=None)
-
+    return quantized_4C2F(nbits=model_word_length,
+                         fbits=model_fractional_bit,
+                         batch_size=batch_size,
+                         quant_mode=None)
+    
 #%%
 # test
 compile_augment={'loss':'categorical_crossentropy','optimizer':'adam','metrics':['accuracy',top2_acc]}
 
-dataset_augment={'dataset':'mnist'}
+dataset_augment={'dataset':'cifar10'}
 
-FT_augment={'model_name':'lenet','loss_function':categorical_crossentropy,'metrics':['accuracy',top2_acc,acc_loss,relative_acc,pred_miss,top2_pred_miss,conf_score_vary_10,conf_score_vary_50]}    
+FT_augment={'model_name':'4c2f','loss_function':categorical_crossentropy,'metrics':['accuracy',top2_acc,acc_loss,relative_acc,pred_miss,top2_pred_miss,conf_score_vary_10,conf_score_vary_50]}    
 
 for concen in concentration_list:
     
     if not os.path.isdir(result_save_folder+'/'+str(concen)):
         os.mkdir(result_save_folder+'/'+str(concen))
-    
+
     for test_rounds,fr in enumerate(fault_rate_list):
         print('|=|=|=|=|=|=|=|=|=|=|=|=|=|=|=|=|=|=|=|=|=|=|=|=|=|=|')
         print('|=|        Test Bit Fault Rate %s'%str(fr))
@@ -97,7 +97,7 @@ for concen in concentration_list:
                                   'weight_fault_dict_list':model_weight_fdl})
     
         result_save_file=result_save_folder+'/'+str(concen)+'/'+str(fr)+'.csv'
-        inference_scheme(quantized_lenet5, 
+        inference_scheme(quantized_4C2F, 
                          model_augment, 
                          compile_augment, 
                          dataset_augment, 
@@ -105,7 +105,7 @@ for concen in concentration_list:
                          weight_load=True, 
                          weight_name=weight_name, 
                          FT_evaluate=True, 
-                         FT_augment=FT_augment, 
+                         FT_augment=FT_augment,
                          name_tag='fault rate '+str(fr))
 
 

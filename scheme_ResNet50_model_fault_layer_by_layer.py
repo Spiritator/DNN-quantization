3c5a3946965af20f4f5249b26f852cf490fb56ac
Spiritator <spiritator@g.ncu.edu.tw>
2019-08-05 15:24:56 +0800

    layer by layer BER implement

--- scheme_ResNet50_model_fault_layer_by_layer.py
+++ scheme_ResNet50_model_fault_layer_by_layer.py
@@ -10,7 +10,7 @@ import os
 
 from simulator.inference.scheme import inference_scheme
 from keras.utils import multi_gpu_model,to_categorical
-from simulator.models.mobilenet import QuantizedMobileNetV1FusedBN, preprocess_input
+from simulator.models.resnet50 import QuantizedResNet50FusedBN, preprocess_input
 from simulator.metrics.topk_metrics import top5_acc
 from keras.losses import categorical_crossentropy
 from simulator.testing.fault_list import generate_model_stuck_fault
@@ -22,12 +22,12 @@ from simulator.metrics.FT_metrics import acc_loss, relative_acc, pred_miss, top5
 img_width, img_height = 224, 224
 
 
-result_save_folder='../test_result/imagenet_mobilenet_model_fault_rate_lbl'
-weight_name='../mobilenet_1_0_224_tf_fused_BN.h5'
+result_save_folder='../test_result/imagenet_resnet_model_fault_rate_lbl'
+weight_name='../resnet50_weights_tf_dim_ordering_tf_kernels_fused_BN.h5'
 set_size=2
 batch_size=40
-model_word_length=16
-model_fractional_bit=9
+model_word_length=[16,16,16]
+model_fractional_bit=[8,12,8]
 rounding_method='nearest'
 if set_size in [50,'full',None]:
     validation_data_dir = '../../dataset/imagenet_val_imagedatagenerator'
@@ -42,12 +42,12 @@ test_round_lower_bound=2
 
 # model for get configuration
 def call_model():
-    return QuantizedMobileNetV1FusedBN(weights=weight_name, 
-                                       nbits=model_word_length,
-                                       fbits=model_fractional_bit, 
-                                       rounding_method=rounding_method,
-                                       batch_size=batch_size,
-                                       quant_mode=None)
+    return QuantizedResNet50FusedBN(weights=weight_name, 
+                                    nbits=model_word_length,
+                                    fbits=model_fractional_bit, 
+                                    rounding_method=rounding_method,
+                                    batch_size=batch_size,
+                                    quant_mode=None)
 
 # layer by layer information
 ref_model=call_model()
@@ -65,7 +65,7 @@ compile_augment={'loss':'categorical_crossentropy','optimizer':'adam','metrics':
 
 dataset_augment={'dataset':'ImageDataGenerator','img_rows':img_width,'img_cols':img_height,'batch_size':batch_size,'data_augmentation':False,'data_dir':validation_data_dir,'preprocessing_function':preprocess_input}
 
-FT_augment={'model_name':'mobilenet','loss_function':categorical_crossentropy,'metrics':['accuracy',top5_acc,acc_loss,relative_acc,pred_miss,top5_pred_miss,conf_score_vary_10,conf_score_vary_50],'fuseBN':True,'setsize':set_size}    
+FT_augment={'model_name':'resnet','loss_function':categorical_crossentropy,'metrics':['accuracy',top5_acc,acc_loss,relative_acc,pred_miss,top5_pred_miss,conf_score_vary_10,conf_score_vary_50],'fuseBN':True,'setsize':set_size}    
 
 for layer_id in param_layers:
     input_bits=param_size_report['input_bits'][layer_id]
@@ -78,7 +78,7 @@ for layer_id in param_layers:
     num_bits=max(input_bits,output_bits,weight_bits)
     
     fault_rate_list,test_rounds_lists=gen_test_round_list(num_bits,test_round_upper_bound,test_round_lower_bound,left_bound=-3,right_bound=-2)    
-    
+
     if not os.path.isdir(result_save_folder+'/'+str(layer_id)):
         os.mkdir(result_save_folder+'/'+str(layer_id))
     
@@ -131,7 +131,7 @@ for layer_id in param_layers:
                                   'weight_fault_dict_list':model_weight_fdl})
     
         result_save_file=result_save_folder+'/'+str(layer_id)+'/'+str(fr)+'.csv'
-        inference_scheme(QuantizedMobileNetV1FusedBN, 
+        inference_scheme(QuantizedResNet50FusedBN, 
                          model_augment, 
                          compile_augment, 
                          dataset_augment, 

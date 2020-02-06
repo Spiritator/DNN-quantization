bf3d7ed8bf99001619cdd03153c22559278c8807
Spiritator <spiritator@g.ncu.edu.tw>
2020-02-06 17:58:17 +0800

    tile data expansion for PE mapping

--- simulator/comp_unit/tile.py
+++ simulator/comp_unit/tile.py
@@ -11,7 +11,7 @@ import numpy as np
 from simulator.memory.tile import tile,tile_FC
 
 class tile_PE(tile):
-    def __init__(self, tile_shape, is_fmap, required_axes=[], axis_prior=[]):
+    def __init__(self, tile_shape, is_fmap, required_axes=[], axis_prior=[], **kwargs):
         """The tile of a DNN feature map or weights
 
         # Arguments
@@ -24,46 +24,26 @@ class tile_PE(tile):
             required_axes: List of Strings. The axis of direction in PE array i.e. 'PE_x', 'PE_y', 't_clk'. 
                 These axes are the dimension in PE array dataflow model for tile mapping.
                 The order in List is the priority for data mapping in PE array.
-            col_prior: Dictionary of List of Strings. The priority of PE mapping in each PE dimension. Consist of 'Tm', 'Tn', 'Tr', 'Tc'.
+            axis_prior: Dictionary of List of Strings. The priority of PE mapping in each PE dimension. Consist of 'Tm', 'Tn', 'Tr', 'Tc'.
                 The dictionary structure is {'PE_x':['Tm', 'Tn', 'Tr', 'Tc'],
                                              'PE_y':['Tm', 'Tn', 'Tr', 'Tc'],
                                              't_clk':['Tm', 'Tn', 'Tr', 'Tc']}
     
         """
-        if not isinstance(is_fmap,bool):
-            raise ValueError('Augment is_fmap must be True (feature map tile) or False (weight tile)')
-        if len(tile_shape) != 4:
-            raise ValueError('The augment tile_shape must be in Tuple dtype and have length 4 but got length %d'%len(tile_shape))
-        if is_fmap:    
-            self.Tm=tile_shape[3]
-            self.Tn=tile_shape[0]
-            self.Tr=tile_shape[1]
-            self.Tc=tile_shape[2]
-        else:
-            self.Tm=tile_shape[2]
-            self.Tn=tile_shape[3]
-            self.Tr=tile_shape[0]
-            self.Tc=tile_shape[1]
-        self.is_fmap=is_fmap
+        super(tile_PE, self).__init__(tile_shape, is_fmap, **kwargs)
+        self.tile_shape=tile_shape
         self.required_axes=required_axes
         self.axis_prior=axis_prior
         self.axis_element=['PE_x','PE_y','t_clk']
         self.prior_element=['Tm','Tn','Tr','Tc']
-        
-        self.fault_dict=dict()
-        self.use_bias=False
-        self.bias_fault_dict=dict()
-        self.shape_len=4
-        
+                
         self.expansion=False
         self.expand_method=None
         self.expand_shape=None
         self.slice_shape=None
         self.slice_permute=None
         self.fault_dict_expand=dict()
-        
-        self.print_detail=True
-        
+                
     def check_prior(self):
         if not isinstance(self.required_axes,list):
             raise ValueError('The augment required_axes must be in list dtype.')
@@ -79,7 +59,7 @@ class tile_PE(tile):
                 if self.axis_prior[axis][i] not in self.prior_element:
                     raise ValueError('The augment axis_prior must be in list %s'%(str(self.prior_element)))
 
-    def reshape_ravel_idx(self,index,source_shape,source_prior,target_shape,targer_prior):
+    def reshape_ravel_idx(self,index,source_shape,source_prior,target_shape,target_prior):
         """ Convert index to differet shapes for tile data expansion. Unravel index to a numtag than ravel to another index.
         
         # Arguments
@@ -87,43 +67,37 @@ class tile_PE(tile):
             source_shape: Tuple. The shape of source array before tranformation.
             source_prior: List or Tuple of Integer. The list for unravel priority of source_shape dimensions. The list is the dimension index.
             target_shape: Tuple. The shape of target array for tranformation to.
-            targer_prior: List or Tuple of Integer. The list for ravel priority of target_shape dimensions. The list is the dimension index.
+            target_prior: List or Tuple of Integer. The list for ravel priority of target_shape dimensions. The list is the dimension index.
 
         """
         if len(source_shape)!=len(source_prior):
             raise ValueError('The length of source_shape must equals to source_prior, but got %d and %d.'%(len(source_shape),len(source_prior)))
         
-        
-        if (isinstance(index,tuple) or (isinstance(index,np.ndarray) and len(index.shape)==1)):
+        restore_index=np.zeros((len(target_shape),),dtype=int)
+        for i in range(len(target_shape)):
+            restore_index[target_prior[i]]=i
+
+        if isinstance(index,tuple) or (isinstance(index,np.ndarray) and len(index.shape)==1):
             if len(index)!=len(source_shape):
                 raise ValueError('The length of coordinate Tuple in tile must be %d but got %d.'%(self.shape_len,len(index)))
 
-            numtag=np.ravel_multi_index(np.array(index)[source_prior],source_shape)
+            numtag=np.ravel_multi_index(np.array(index)[source_prior],np.array(source_shape)[source_prior])
             
+            coor=np.unravel_index(numtag,np.array(target_shape)[target_prior])
+            coor=np.array(coor)[restore_index]
+            
+            return tuple(coor)
                         
         elif isinstance(index,np.ndarray):
             if index.shape[-1]!=self.shape_len:
                 raise ValueError('The length of coordinate Tuple in tile must be %d but got %d.'%(self.shape_len,index.shape[-1]))
                 
-            numtag=np.ravel_multi_index(index.T[source_prior],source_shape)
+            numtag=np.ravel_multi_index(index.T[source_prior],np.array(source_shape)[source_prior])
                     
-        
-        restore_index=np.zeros((self.shape_len,),dtype=int)
-        for i in range(len(target_shape)):
-            restore_index[targer_prior[i]]=i
-        
-        if isinstance(numtag,np.ndarray):
-            coor=np.unravel_index(numtag,target_shape)
+            coor=np.unravel_index(numtag,np.array(target_shape)[target_prior])
             coor=np.array(coor)[restore_index]
 
             return coor.T
-        
-        else:
-            coor=np.unravel_index(numtag,target_shape)
-            coor=np.array(coor)[restore_index]
-            
-            return tuple(coor)
-
 
     def expand_data(self, method, expect_shape, slice_dims, slices_permute):
         """ Data expansion before put into PE array. Usually used for ifmap and weight reuse. 
